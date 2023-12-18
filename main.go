58c3f42b5d9f28c58bc172bdeee5c86275f4f6ba
matteus8 <camachomatteus@gmail.com>
2023-12-18 14:48:38 -0700

    huge refactor into gin for webapp frontend

--- main.go
+++ main.go
@@ -2,73 +2,81 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
+// Conference represents the conference details
+type Conference struct {
+	Name           string
+	TicketAmount   uint
+	RemainingTickets uint
+	Bookings       []string
+}
+
+var conference = Conference{
+	Name:           "Go-lang Conference",
+	TicketAmount:   50,
+	RemainingTickets: 50,
+	Bookings:       []string{},
+}
+
 func main() {
-	// Define conferenceName as a constant string
-	const conferenceName = "Go-lang Conference"
+	// Create a new Gin router
+	router := gin.Default()
 
-	// Define conferenceTicketAmount as a constant unsigned integer
-	const conferenceTicketAmount uint = 50
+	// Set the path to the templates folder
+	router.LoadHTMLGlob("templates/*")
 
-	// Initialize remainingTickets with the initial available tickets
-	var remainingTickets uint = 50
+	// Set up routes
+	router.GET("/", showHomePage)
+	router.POST("/book", bookTickets)
 
-	fmt.Printf("\nWelcome to the %v booking application!\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets, and %v available.\n", conferenceTicketAmount, remainingTickets)
-	fmt.Printf("Get your tickets here.\n")
+	// Start the server
+	router.Run(":8080")
+}
 
-	// Create a slice (an abstraction of an array) for storing bookings
-	var allBookings = []string{}
+func showHomePage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"ConferenceName":     conference.Name,
+		"ConferenceCapacity": conference.TicketAmount,
+		"RemainingTickets":   conference.RemainingTickets,
+	})
+}
 
-	// Continue taking user information and booking tickets until the conference is fully booked
-	for remainingTickets > 0 {
-		// Get user's first name
-		var firstName string
-		fmt.Printf("Please enter your first name... \n")
-		fmt.Scan(&firstName)
+func bookTickets(c *gin.Context) {
+	// Parse form data
+	firstName := c.PostForm("firstName")
+	lastName := c.PostForm("lastName")
+	email := c.PostForm("email")
+	ticketsStr := c.PostForm("tickets")
 
-		// Get user's last name
-		var lastName string
-		fmt.Printf("Please enter your last name...\n")
-		fmt.Scan(&lastName)
-
-		// Get user's email
-		var userEmail string
-		fmt.Printf("Please enter your email..\n")
-		fmt.Scan(&userEmail)
-
-		// Get the number of tickets the user wants
-		var userTickets uint
-		fmt.Printf("Please enter how many tickets you would like...")
-		fmt.Scan(&userTickets)
-		fmt.Printf("\n\n\n")
-
-		// Check if there are enough tickets available
-		if userTickets > remainingTickets {
-			fmt.Printf("We only have %v tickets remaining, please try again.\n", remainingTickets)
-			continue // Continue to the next iteration of the loop
-		}
-
-		// Print statements for booking
-		fmt.Printf("User %v, %v, booked %v tickets\n", firstName, lastName, userTickets)
-		fmt.Printf("Thank you %v, %v, for booking %v tickets\n", firstName, lastName, userTickets)
-		fmt.Printf("We will send an email to %v for confirmation.\n\n", userEmail)
-
-		// Adjust remainingTickets after booking
-		remainingTickets = remainingTickets - userTickets
-		fmt.Printf("There are %v remaining tickets for the %v conference\n\n", remainingTickets, conferenceName)
-
-		// Append user's full name to the slice
-		allBookings = append(allBookings, fmt.Sprintf("%v %v - Email: %v, Tickets: %v", firstName, lastName, userEmail, userTickets))
+	// Convert tickets to uint
+	tickets, err := strconv.ParseUint(ticketsStr, 10, 32)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Invalid number of tickets")
+		return
 	}
 
-	// Print a message when the conference is fully booked
-	fmt.Printf("Our conference is fully booked. Hopefully next year is better.\n\n")
-
-	// Print the list of users
-	fmt.Println("List of Users:")
-	for _, user := range allBookings {
-		fmt.Println(user)
+	// Check if there are enough tickets available
+	if uint(tickets) > conference.RemainingTickets {
+		c.String(http.StatusBadRequest, "Not enough tickets available")
+		return
 	}
+
+	// Book tickets
+	bookingDetails := fmt.Sprintf("%v %v - Email: %v, Tickets: %v", firstName, lastName, email, tickets)
+	conference.Bookings = append(conference.Bookings, bookingDetails)
+	conference.RemainingTickets -= uint(tickets)
+
+	// Display confirmation message
+	c.HTML(http.StatusOK, "confirmation.html", gin.H{
+		"FirstName":          firstName,
+		"LastName":           lastName,
+		"BookedTickets":      tickets,
+		"RemainingTickets":   conference.RemainingTickets,
+		"ConferenceName":     conference.Name,
+		"BookingConfirmation": "Booking successful! We will send an email for confirmation.",
+	})
 }
