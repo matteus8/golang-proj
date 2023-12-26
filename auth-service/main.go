0e563d5e3dd881c58f60c68cc1c297d6fdade9da
matteus8 <camachomatteus@gmail.com>
2023-12-26 11:53:47 -0700

    added commnets

--- auth-service/main.go
+++ auth-service/main.go
@@ -1,3 +1,25 @@
+// main.go is the main entry point for the auth service. It initializes 
+// the router, configures routes, and starts the HTTP server.
+
+// main initializes the Gin router, configures routes, and starts the server.
+func main() {
+
+// register handles user registration.
+func register(c *gin.Context) {
+
+// login handles user login and generates a JWT token.  
+func login(c *gin.Context) {
+
+// generateToken signs a JWT token using the provided username and secret key.
+func generateToken(username string) string {
+// main.go is the entry point for the auth service. It initializes the router,
+// configures routes, and starts the HTTP server.
+
+// register handles user registration.
+
+// login handles user login and generates a JWT token. 
+
+// generateToken signs a JWT token using the provided secret key.
 // main.go
 package main
 
@@ -9,6 +31,9 @@ import (
 
 var secretKey = []byte("your-secret-key") // Change this to a secure secret key
 
+// main is the entry point for the application. It initializes the Gin router, 
+// configures the routes for the /register and /login endpoints, and starts 
+// the HTTP server on port 8081.
 func main() {
     router := gin.Default()
 
